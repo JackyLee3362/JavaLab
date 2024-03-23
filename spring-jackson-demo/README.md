0496e4ff2a02700958a8231fc51a9803345389f8
Jackylee <779557618@qq.com>
2024-03-23 14:19:34 +0800

    add-project 添加 spring-jackson-demo 项目源码

---
+++ spring-jackson-demo/README.md
@@ -0,0 +1,8 @@
+# jackson 测试
+
+## 实验
+
+测试 Jackson 的 ObjectMapper
+
+[测试代码](src/test/java/org/example/TestJackson.java)
+
---
+++ spring-jackson-demo/src/main/java/org/example/Teacher.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Teacher {
+    public String name;
+    public List<Info> info;
+
+    @Data
+    public static class Info{
+        public String key1;
+    }
+}
